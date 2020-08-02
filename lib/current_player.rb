788ac49ffc0f46ad443a1f0fc06b4c47777b0025
Caroline Forrester <carolineforrester22@gmail.com>
2020-08-02 20:14:32 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,11 @@ def current_player
 end
 
 def turn_count(board)
+  turn = 0 
   board.each do |space|
     if
       space == "X" || space == "O"
       turn = turn + 1 
     end
+    return turn 
 end
